016fda99f139acab02e9e9bcb214cf24eecdd506
KalebeSouza <kalebe.tsi@gmail.com>
2018-05-17 08:48:33 -0300

    Commit disciplinas
    commit das disciplinas geografia e história. Criei o mursal de mensagens específica, área de dúvidas e especificações do professor.

---
+++ web/disciplinas/hist.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<title>Historia</title>
+<link rel="icon" href="../img/hist.png" type="image/x-icon" />
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../css/w3.css">
+<link rel='stylesheet' href='../css/font.css'>
+<script type="text/javascript" src="../js/javaScript.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+<style>
+html,header,body,h1,h2,h3,h4,h5,h6, {font-family: "Roboto", sans-serif}
+
+</style>
+
+<body class="w3-light-grey">
+<form>
+
+<div class="w3-content w3-margin-top" style="max-width:1400px;" id="teste">
+
+
+  <div class="w3-row-padding">
+  
+
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="../img/profHist.png" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-ligt-black" style="font-family: Arial, sans-serif;">
+            <h2>Professora: Mônica</h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <center><p><i class="fa fa-info fa-fw w3-margin-right w3-large w3-text-teal"></i>Informações sobre a professora</p></center>
+          <hr>
+          <p><i class="fa fa-mortar-board fa-fw w3-margin-right w3-large w3-text-teal"></i>Formação Academica: História - UniCEUB</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>port.hist@espacocademico.com</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>(61) 3333-7777</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Frequência na Disciplina</b></p>
+          
+          <p>História</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:95%">
+              <div class="w3-center w3-text-white">95%</div>
+            </div>
+          </div>    
+          <br>
+        </div>
+      </div>
+      <br>
+    </div>
+
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+  
+        <h2 class="w3-text-grey w3-padding-16">
+          <img src="../img/hist.png" class="w3-circle w3-grayscale-max w3-margin-right w3-xxlarge w3-text-teal" style="width: 10%;" alt="Norway"> Disciplina: História  
+           <i class="fa fa-angle-double-left fa-fw w3-border-white w3-xxlarge" onclick="goToHome()" style="margin-left: 49%;">    
+         </i> 
+        </h2>
+  
+
+        <div class="w3-container">
+          <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-envelope fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Mesagens da Disciplina</h2> 
+            <h5 class="w3-opacity">
+              <b>Passeio - Museu</b>
+            </h5>
+          <h6 class="w3-text-teal">
+            <i class="fa fa-calendar fa-fw w3-margin-right"></i>27 Maio 2018 - <span class="w3-tag w3-teal w3-round">Aberto</span></h6>
+          <p>Partida: 08hrs - Chegada: 17hrs.</p>
+          <p>Valor: R$ 25,00</p>
+          <hr>
+        </div>
+        <hr>
+        <div class="w3-container w3-card-4 w3-white w3-margin-bottom">
+          <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-calendar-check-o fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>
+            Dúvidas?
+          </h2> 
+          <div class="w3-container" >
+            <textarea rows="5" placeholder="Envie uma mensagem para sua Professora" style="width: 100%; resize: none;"> 
+            </textarea>
+            <br>
+            <br>
+            <button class="w3-btn w3-teal w3-round-xlarge w3-right w3-padding-small"><h4> Enviar </h4></button>
+
+          </div> 
+            <br>     
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</form>
+</body>
+</html>
