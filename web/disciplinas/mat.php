b97bdfd47de83c8f1d49f83757cdab385ddd776c
SousAlves <sousalves.as@gmail.com>
2018-05-16 22:59:47 -0300

    Disciplinas
    Commitei a tela das disciplinas de port e matematica, coloquei a area de duvidas, e as mensagens de cada materia especificas, adcionado tambem o quantidade % de falta do aluno na disciplina.

---
+++ web/disciplinas/mat.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<title>Matemática</title>
+<link rel="icon" href="../img/math.png" type="image/x-icon" />
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../css/w3.css">
+<link rel='stylesheet' href='../css/font.css'>
+<script type="text/javascript" src="../js/javaScript.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+<style>
+html,header,body,h1,h2,h3,h4,h5,h6, {font-family: "Roboto", sans-serif}
+
+</style>
+
+<body class="w3-light-grey">
+<form>
+
+<div class="w3-content w3-margin-top" style="max-width:1400px;" id="teste">
+
+
+  <div class="w3-row-padding">
+  
+
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="../img/profMat.png" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-ligt-black" style="font-family: Arial, sans-serif;">
+            <h2>Professora: Maria</h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <center><p><i class="fa fa-info fa-fw w3-margin-right w3-large w3-text-teal"></i>Informações sobre o professor</p></center>
+          <hr>
+          <p><i class="fa fa-mortar-board fa-fw w3-margin-right w3-large w3-text-teal"></i>Formação Academica: Matemática - UCB</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>port.mat@espacocademico.com</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>(61) 3333-5555</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Frequência na Disciplina</b></p>
+          
+          <p>Matemática</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-red" style="width:26%">
+              <div class="w3-center w3-text-white">26%</div>
+            </div>
+          </div>    
+          <br>
+        </div>
+      </div>
+      <br>
+    </div>
+
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+  
+        <h2 class="w3-text-grey w3-padding-16">
+          <img src="../img/math.png" class="w3-circle w3-grayscale-max w3-margin-right w3-xxlarge w3-text-teal" style="width: 10%;" alt="Norway"> Disciplina: Matemática  
+           <i class="fa fa-angle-double-left fa-fw w3-border-white w3-xxlarge" onclick="goToHome()" style="margin-left: 45%;">    
+         </i> 
+        </h2>
+  
+
+        <div class="w3-container">
+          <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-envelope fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Mesagens da Disciplina</h2> 
+            <h5 class="w3-opacity">
+              <b>Prova - Matemática</b>
+            </h5>
+          <h6 class="w3-text-teal">
+            <i class="fa fa-calendar fa-fw w3-margin-right"></i>06 Junho 2018 - <span class="w3-tag w3-teal w3-round">Aberto</span></h6>
+          <p>Conteúdo: Potência, raízes, frações.</p>
+          <hr>
+
+          <h2 class="w3-text-grey w3-padding-10">
+            <h5 class="w3-opacity">
+              <b>Trabalho - Apresentação</b>
+            </h5>
+          <h6 class="w3-text-teal">
+            <i class="fa fa-calendar fa-fw w3-margin-right"></i>12 Maio 2018 - <span class="w3-tag w3-red w3-round">Encerrado</span></h6>
+          <p>Tema: Como surgiu a matemática?.</p>
+        </div>
+        <hr>
+        <div class="w3-container w3-card-4 w3-white w3-margin-bottom">
+          <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-calendar-check-o fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>
+            Dúvidas?
+          </h2> 
+          <div class="w3-container" >
+            <textarea rows="5" placeholder="Envie uma mensagem para sua Professora" style="width: 100%; resize: none;"> 
+            </textarea>
+            <br>
+            <br>
+            <button class="w3-btn w3-teal w3-round-xlarge w3-right w3-padding-small"><h4> Enviar </h4></button>
+
+          </div> 
+            <br>     
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</form>
+</body>
+</html>
---
+++ web/disciplinas/port.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<title>Português</title>
+<link rel="icon" href="../img/port.png" type="image/x-icon" />
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../css/w3.css">
+<link rel='stylesheet' href='../css/font.css'>
+<script type="text/javascript" src="../js/javaScript.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+<style>
+html,header,body,h1,h2,h3,h4,h5,h6, {font-family: "Roboto", sans-serif}
+
+</style>
+
+<body class="w3-light-grey">
+<form>
+
+<div class="w3-content w3-margin-top" style="max-width:1400px;" id="teste">
+
+
+  <div class="w3-row-padding">
+  
+
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="../img/profPort.png" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-ligt-black" style="font-family: Arial, sans-serif;">
+            <h2>Professor: Bruno Silva</h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <center><p><i class="fa fa-info fa-fw w3-margin-right w3-large w3-text-teal"></i>Informações sobre o professor</p></center>
+          <hr>
+          <p><i class="fa fa-mortar-board fa-fw w3-margin-right w3-large w3-text-teal"></i>Formação Academica: Letras - UnB</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>port.prof@espacocademico.com</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>(61) 3333-4587</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Frequência na Disciplina</b></p>
+          
+          <p>Português</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
+            </div>
+          </div>    
+          <br>
+        </div>
+      </div>
+      <br>
+    </div>
+
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+  
+        <h2 class="w3-text-grey w3-padding-16">
+          <img src="../img/port.png" class="w3-circle w3-grayscale-max w3-margin-right w3-xxlarge w3-text-teal" style="width: 10%;" alt="Norway"> Disciplina: Português  
+           <i class="fa fa-angle-double-left fa-fw w3-border-white w3-xxlarge" onclick="goToHome()" style="margin-left: 49%;">    
+         </i> 
+        </h2>
+  
+
+        <div class="w3-container">
+          <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-envelope fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Mesagens da Disciplina</h2> 
+          <h5 class="w3-opacity">
+          <b>Prova - Gramática</b>
+            </h5>
+          <h6 class="w3-text-teal">
+            <i class="fa fa-calendar fa-fw w3-margin-right"></i>05 Junho 2018 - <span class="w3-tag w3-teal w3-round">Aberto</span></h6>
+          <p>Conteúdo: Tempos verbais, conjugações, preposição.</p>
+          <hr>
+
+          <h2 class="w3-text-grey w3-padding-10">
+            <h5 class="w3-opacity">
+              <b>Reuniões de Pais</b>
+            </h5>
+          <h6 class="w3-text-teal">
+            <i class="fa fa-calendar fa-fw w3-margin-right"></i>11 Maio 2018 - <span class="w3-tag w3-red w3-round">Encerrado</span></h6>
+          <p>Horário: 14hrs - Sala 1001.</p>
+        </div>
+        <hr>
+        <div class="w3-container w3-card-4 w3-white w3-margin-bottom">
+          <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-calendar-check-o fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>
+            Dúvidas?
+          </h2> 
+          <div class="w3-container" >
+            <textarea rows="5" placeholder="Envie uma mensagem para seu Professor" style="width: 100%; resize: none;"> 
+            </textarea>
+            <br>
+            <br>
+            <button class="w3-btn w3-teal w3-round-xlarge w3-right w3-padding-small"><h4> Enviar </h4></button>
+
+          </div> 
+            <br>     
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</form>
+</body>
+</html>
