3f8ece6704528cea17e45690441967e9bcb6b78f
Victor Henrique <victorhrqs@gmail.com>
2018-05-16 22:54:49 -0300

    Js
    javascript da aplicação

---
+++ web/js/javaScript.js
@@ -0,0 +1,30 @@
+
+// function funcao(){
+	
+// 	$.ajax({
+// 		url: '../template/modalDisciplina.php',
+// 		success: function(data){
+// 			$('#div_conteudo').html(data);
+// 		}
+// 	});
+// }
+
+function goToHome(){
+	window.location.href = "../index.php";
+}
+
+function portugues(){
+	window.location.href = "../disciplinas/port.php";
+}
+
+function matematica(){
+	window.location.href = "../disciplinas/mat.php";
+}
+
+function historia(){
+	window.location.href = "../disciplinas/hist.php";
+}
+
+function geografia(){
+	window.location.href = "../disciplinas/geo.php";
+}
