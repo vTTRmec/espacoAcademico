b693bf8362b84ed3f24f4b5be0ca45fb2f3260c3
KalebeSouza <kalebe.tsi@gmail.com>
2018-06-06 20:51:14 -0300

    commit professor
    Informações do professor

---
+++ web/professor.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<title>Espaço Academico</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/w3.css">
+<link rel='stylesheet' href='css/font.css'>
+<script type="text/javascript" src="js/javaScript.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+<style>
+html,header,body,h1,h2,h3,h4,h5,h6,{font-family: "Roboto", sans-serif}
+
+img {
+    width: 12%;
+    height: auto;
+} 
+
+</style>
+<form>
+<body class="w3-light-grey">
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+
+  <div class="w3-row-padding">
+  
+
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="img/profGeo.png" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-ligt-black" style="font-family: Arial, sans-serif;">
+            <h2>Professora: Marina</h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          
+          <p><i class="fa fa-graduation-cap fa-fw w3-margin-right w3-large w3-text-teal"></i>Universidade de Brasília - Geografia</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>professora.geo@espacocademico.com</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>(61) 2222-4444</p>
+          <hr>
+      
+          <div>
+          <button onclick="location.href='index.php'" type="button" class="w3-btn w3-red w3-border w3-border-red w3-round-large" style="margin-left: 35%;">Sair</button>
+          </div>
+           <br>
+        </div>
+
+
+      </div><br>
+
+    	</div>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+</form>
+</html>
+
