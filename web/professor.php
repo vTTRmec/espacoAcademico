38815cf09e8dfab6fdc84f7b4c204be247dad47c
Victor Henrique <victorhrqs@gmail.com>
2018-06-06 20:58:00 -0300

    commit chamada
    commit dos alunos e suas a opçao de dar falta e presença para o mesmo.

--- web/professor.php
+++ web/professor.php
@@ -50,6 +50,49 @@ img {
 
       </div><br>
 
+      <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa fa-address-card-o fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Chamada</h2> 
+        <div class="w3-container">
+
+         <div>
+          <h3 ><img src="img/profHist.png" class="w3-circle" style="margin-right: 2%;">Maria Silva
+          <i id="iconChamada" class="fa fa-check fa-fw w3-xxlarge w3-text-grey" style="margin-left: 55.5%;" onclick="chamada('presente');"> 
+          </i>
+          <i id="iconChamadaFalta" class="fa fa-times fa-fw w3-xxlarge w3-text-grey" style="margin-top: -5%; margin-left: 2%;" onclick="chamada('ausente');"> 
+          </i>
+
+          </h3>
+          
+       
+          <h3 ><img src="img/profPort.png" class="w3-circle" style="margin-right: 2%;">Joao Carlos
+          <i id="iconChamada1" class="fa fa-check fa-fw w3-margin-right w3-xxlarge w3-text-grey" style="margin-left: 54.5%;" onclick="chamada1('presente');"> 
+          </i>
+          <i id="iconChamadaFalta1" class="fa fa-times fa-fw w3-xxlarge w3-text-grey" style="margin-top: -5%;" onclick="chamada1('ausente');"> 
+            </i> 
+          </h3>
+          
+    
+          <h3 ><img src="img/profMat.png" class="w3-circle w3-xxlarge" style="margin-right: 2%;">Marina 
+          <i id="iconChamada2" class="fa fa-check fa-fw w3-margin-right w3-xxlarge w3-text-grey" style="margin-left: 60.2%;" onclick="chamada2('presente');">
+          </i>
+          <i id="iconChamadaFalta2" class="fa fa-times fa-fw w3-xxlarge w3-text-grey" style="margin-top: -5%; margin-right: 1%;" onclick="chamada2('ausente');"> 
+            </i> 
+          </h3>
+          
+         </div>
+          <div>
+
+            <h3 id="textoChamada" class="w3-center" style="display: block;"></h3>
+              <h5 class="w3-btn w3-round-xxlarge w3-teal" onclick="botaoConfirmar()" style="margin-left: 75%;">Confirmar</h5>
+              <h5 class="w3-btn w3-round-xxlarge w3-red" onclick="botaoCancelar()" style="margin-left: 90%;margin-top: -8.9%;">Cancelar</h5>
+            
+          </div>
+          <hr>
+        </div>
+        </div>
+        </div>
     	</div>
     </div>
   </div>
